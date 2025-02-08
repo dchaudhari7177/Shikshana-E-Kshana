612f619e2a72726fc9fe3949f332174632afbf6f
Dipak <dipakchaudhari171@gmail.com>
2025-02-08 17:53:25 +0530

    Options page added

--- app/src/main/java/com/example/shikshana_e_kshana/OptionActivity.java
+++ app/src/main/java/com/example/shikshana_e_kshana/OptionActivity.java
@@ -1,6 +1,9 @@
 package com.example.shikshana_e_kshana;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,22 @@ public class OptionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_option);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Find button1 by ID and set an OnClickListener
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open ChatbotActivity when button is clicked
+                Intent intent = new Intent(OptionActivity.this, ChatbotActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
