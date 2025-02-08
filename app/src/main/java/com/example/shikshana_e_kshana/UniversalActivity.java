0beeed892e139613ca2dfe8f0ec968997c49f37f
Dipak <dipakchaudhari171@gmail.com>
2025-02-08 20:27:34 +0530

    content activity added

--- app/src/main/java/com/example/shikshana_e_kshana/UniversalActivity.java
+++ app/src/main/java/com/example/shikshana_e_kshana/UniversalActivity.java
@@ -1,8 +1,6 @@
 package com.example.shikshana_e_kshana;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class EnglishOption extends AppCompatActivity {
+public class UniversalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_english_option);
+        setContentView(R.layout.activity_universal);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button seven = findViewById(R.id.class7Button);
-        if (seven != null) {
-            seven.setOnClickListener(v -> {
-                Intent intent = new Intent(EnglishOption.this, UniversalActivity.class);
-                startActivity(intent);
-            });
-        } else {
-            System.out.println("user_profile ImageView not found!");
-        }
     }
 }
